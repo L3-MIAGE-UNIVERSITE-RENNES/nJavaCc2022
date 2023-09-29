25820e1e827bf351771ac3fccb3fd12fb59b2df3
troemmnuel <trohkopeemmanuel@gmail.com>
2023-09-29 03:13:45 +0200

    Exercice1-commune

---
+++ src/Commune/Commune.java
@@ -0,0 +1,69 @@
+package Commune;
+
+public class Commune {
+    private String name;
+    private int department;
+    private int postalCode;
+    private int surface;
+    private int population;
+
+    // constructors
+    public Commune(){};
+    public Commune(String name, int department, int postalCode, int surface, int population){
+        try {
+            if(department < 1 || department > 99){
+              throw new IllegalAccessException("department should be between 1 and 99");
+            }
+
+            if(surface < 0 || population < 0 ){
+                throw new IllegalAccessException("Ce nombre est strictememnt positif");
+            }
+
+            if(department != postalCode / 1000 ){
+                throw new IllegalAccessException("Le departement ne corresponds pas au code postal");
+            }
+
+            this.name = name;
+            this.department = department;
+            this.population = population;
+            this.surface = surface;
+            this.postalCode = postalCode;
+
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // setters
+    public String getName(){
+        return this.name;
+    }
+    public int getDepartment(){
+        return this.department;
+    }
+    public int getPostalCode(){
+        return this.postalCode;
+    }
+    public int getSurface(){
+        return this.surface;
+    }
+    public int getPopulation(){
+        return this.population;
+    }
+
+    public String toString(){
+        return "name: " + this.name + ", department: " + this.department + ", postalCode: " + this.postalCode + ", surface: " + this.surface + ", population: " + this.population;
+    }
+
+    public static Commune morePopulated(Commune commune1, Commune commune2){
+        if(commune1.population > commune2.population) {
+            return commune1;
+        }
+        return commune2;
+    }
+
+    public double density(){
+        return (double) this.population/this.surface;
+    }
+}
+
---
+++ src/Commune/Main.java
@@ -0,0 +1,41 @@
+package Commune;
+
+public class Main {
+    public static void main(String[] args){
+          Commune commune1 = new Commune("Saint-Nazaire", 46, 46600, 23990,100356);
+          Commune commune2 = new Commune("Saint-Nazaire", 42, 42300, 207780,322462);
+          Commune commune3 = new Commune("Rennes", 35, 35000, 2000,1000000);
+
+          Commune morePopulated = Commune.morePopulated(commune1, commune3);
+          System.out.println("More populated -> " + morePopulated);
+          Commune[] array = {commune1, commune2, commune3};
+          Commune leastPopulated = leastPopulated(array);
+          System.out.println("Least populated -> " + leastPopulated);
+          int sameName = sameName(array, "Saint-Nazaire");
+          System.out.println("Same name -> " + sameName);
+    }
+
+    public static Commune leastPopulated(Commune[] array){
+        int i = 0;
+        Commune leastPopulated = array[0];
+        while(i < array.length){
+            if(leastPopulated.density() > array[i].density()){
+                leastPopulated = array[i];
+            }
+            i++;
+        }
+        return leastPopulated;
+    }
+
+    public static int sameName(Commune[] array, String name){
+        int i = 0;
+        int count = 0;
+        while(i < array.length){
+            if(array[i].getName().equals(name)){
+                count = count + 1;
+            }
+            i++;
+        }
+        return count;
+    }
+}
